cfdd775ed6ddc4df93404d1f3adfbace2c7d56ae
RendaniTshinakaho <r.nkhumeleni@gmail.com>
2017-09-10 13:21:00 +0200

    First Commit

---
+++ StringCalculator/StringCalculator.Tests/CalculatorTest.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTest
+    {
+        private ICalculator _calculator;
+        [SetUp]
+        public void Init()
+        {
+            _calculator = new Calculator();
+        }
+        [Test]
+        public void CanInstantiateClass()
+        {
+            Assert.That(_calculator,Is.Not.Null);
+        }
+
+        [Test]
+        public void GivenAnEmptyStringReturnZero()
+        {
+            var result = _calculator.Add("");
+            Assert.That(result,Is.EqualTo(0));
+        }
+
+        [TestCase("1",1)]
+        [TestCase("2",2)]
+        [TestCase("3",3)]
+        public void GivenANumberReturnsValue(string input,int expected)
+        {
+            var result = _calculator.Add(input);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+
+        [TestCase("1,2",3)]
+        [TestCase("2,2",4)]
+        [TestCase("3,2",5)]
+        public void GivenACommaDelimeterNumberReturnsValue(string input,int expected)
+        {
+            var result = _calculator.Add(input);
+            Assert.That(result,Is.EqualTo(expected));
+        }
+
+        [TestCase("1\n2,3", 6)]
+        public void GivenANumberSeparatedByCommaDelimeter(string input,int expected)
+        {
+            var result = _calculator.Add(input);
+            Assert.That(result,Is.EqualTo(expected));
+        }
+    }
+}
---
+++ StringCalculator/StringCalculator/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator
+{
+    public class Calculator : ICalculator
+    {
+        List<char> Delimeter = new List<char> { ',', '\n' };
+        public int Add(string numbers)
+        {
+            if (string.IsNullOrEmpty(numbers))
+            {
+                return 0;
+            }
+            var sum = numbers.Split(Delimeter.ToArray()).Select(x => int.Parse(x)).Sum();
+            return sum;
+        }
+    }
+}
